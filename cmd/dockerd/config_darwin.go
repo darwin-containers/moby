290ebc9e0937083a9b3bf48a6e73c67f2f3537a0
Marat Radchenko <marat@slonopotamus.org>
2023-09-25 21:51:41 +0300

    Add flag to specify Unix socket group

--- cmd/dockerd/config_darwin.go
+++ cmd/dockerd/config_darwin.go
@@ -14,6 +14,7 @@ func installConfigFlags(conf *config.Config, flags *pflag.FlagSet) error {
 	}
 
 	flags.Var(opts.NewNamedRuntimeOpt("runtimes", &conf.Runtimes, config.StockRuntimeName), "add-runtime", "Register an additional OCI compatible runtime")
+	flags.StringVarP(&conf.SocketGroup, "group", "G", "docker", "Group for the unix socket")
 
 	return nil
 }
